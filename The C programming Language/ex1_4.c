72d9e3888a26766ddb5ab8550e34a7d0a91de191
HawSJun <ghkdtjrwns12@naver.com>
2025-03-16 12:34:00 +0900

    C 예제 연습

---
+++ The C programming Language/ex1_4.c
@@ -0,0 +1,27 @@
+﻿/*
+	섭씨(c)를 화씨(f)로 바꾸는 프로그램 작성
+	fahrenheit = (celsius * 9.0 / 5.0) + 32;
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	celsius = lower;
+	printf("Celsius   Fahr\n");
+
+	while (celsius <= upper) {
+		fahr = (celsius * 9.0 / 5.0) + 32.0;
+		printf("%3.0f\t%6.1f\n", celsius, fahr);
+		celsius += step;
+	}
+
+	return 0;
+}
---
+++ The C programming Language/ex1_5.c
@@ -0,0 +1,46 @@
+﻿/*
+	ex1_4에서 화씨온도가 0일때부터 300일 때까지 계산했다.
+	300도에서 0도까지를 계산하는 프로그램을 작성해봐라. 즉, 온도 변환표를 역순으로 만들어라
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	int fahr;
+
+	for (fahr = 300; fahr >= 0; fahr -= 20) {
+		printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32.0));
+	}
+	return 0;
+}
+
+#include <stdio.h>
+
+int main()
+{
+	int fahr;
+	int lower = 0;
+	int step = 20;
+
+	for (fahr = 300; fahr >= lower; fahr -= step) {
+		printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32.0));
+	}
+	return 0;
+}
+
+
+#include <stdio.h>
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
+int main()
+{
+	int fahr;
+
+	for (fahr = LOWER; fahr >= UPPER; fahr -= STEP) {
+		printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32.0));
+	}
+	return 0;
+}
