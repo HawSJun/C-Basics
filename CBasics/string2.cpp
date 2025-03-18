9a30456d7bccd857d295bc90e29e17daf516f719
HawSJun <ghkdtjrwns12@naver.com>
2025-03-18 19:57:01 +0900

    C/C++

---
+++ CBasics/string2.cpp
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <mbstring.h>
+#include <wchar.h>
+
+unsigned int GetLength(const wchar_t* _pStr)
+{
+	int i = 0;	// 문자 갯수 체크 용도
+
+	while ('\0' != _pStr[i])
+	{	
+		//wchar_t c = *(_pStr + i);		// _pStr[i]
+
+		/*if ('\0' == c)
+		{
+			break;
+		}*/
+
+		++i;
+	}
+	return i;
+}
+
+int main()
+{
+	wchar_t szName[10] = L"Raimond";		// 문자열은 읽기전용 메모리에 있음.(데이터 초기화영역) 
+
+	//int iLen = wcslen(szName);
+	
+	// 문자열 길이 
+	int iLen = GetLength(szName);
+	printf("%d", iLen);
+
+	/*char sName[10] = "Raimond";
+	int clen = strlen(sName);*/
+
+	// 문자열 이어 붙이기
+	wcscat_s()
+	return 0;
+}
